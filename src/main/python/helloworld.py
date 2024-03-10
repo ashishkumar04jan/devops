ccacac43469ffc86d61dc2a06c2f15bc0586ce7d
ashishkumar04jan <93425238+ashishkumar04jan@users.noreply.github.com>
2024-03-10 22:39:32 +0530

    removed recursion

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -7,4 +7,4 @@ This is a simple module to demonstrate a hello world function in Python.
 import sys
 
 def helloworld(out):
-    out.write("Hello  world of Python\n")
+    out.write("Hello world of Python\n")
